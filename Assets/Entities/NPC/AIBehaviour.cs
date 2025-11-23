9bf11a11993f246a56255c3869e79e9510f89e1d
NuTrynity <88869373+NuTrynity@users.noreply.github.com>
2025-11-23 15:04:56 +0800

    added NPC

---
+++ Assets/Entities/NPC/AIBehaviour.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class AIBehaviour : MonoBehaviour
+{
+    public Transform target;
+
+    /*
+        TODO:
+            If npc is near the target, switch to chase State
+            if npc is near attacking range of target, stop moving and switch to attack state
+            if target is outside NPC Range, stop moving and switch to idle state
+    */
+}
