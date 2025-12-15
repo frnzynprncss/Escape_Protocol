be53a69245cc1df1e95fa18296fe8f37cea22230
frnzynprncss <160759074+frnzynprncss@users.noreply.github.com>
2025-12-15 12:29:43 +0800

    added some parts

--- Assets/Entities/Player/MainMenu_Script.cs
+++ Assets/Entities/Player/MainMenu_Script.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("PlayerTestScene");
     }
 
     public void QuitGame()
